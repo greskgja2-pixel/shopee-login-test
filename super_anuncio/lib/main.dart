81acd5ba60e02aaf9f2eb99b2b1457eb6fb7c6e7
greskgja2-pixel <greskgja2@gmail.com>
2026-09-11 21:24:40 -0300

    feat: ativa fluxo de verificacao Shopee

--- super_anuncio/lib/main.dart
+++ super_anuncio/lib/main.dart
@@ -21,6 +21,7 @@ part 'shopee_service.dart';
 part 'shopee_connection.dart';
 part 'shopee_web_collector.dart';
 part 'shopee_web_collector_v2.dart';
+part 'shopee_verification_gate.dart';
 part 'gemini_service.dart';
 part 'engine.dart';
 part 'competitor_widgets.dart';
